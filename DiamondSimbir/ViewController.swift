e847d1daee1f993f8e0081a0334a0cd0c4d35d02
Evgeniy <gafter921@yandex.ru>
2021-11-16 18:03:09 +0300

    mb final desine

--- DiamondSimbir/ViewController.swift
+++ DiamondSimbir/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Button(_ sender: Any) {
+        label.text = "osX"
+    }
+    
 }
 
